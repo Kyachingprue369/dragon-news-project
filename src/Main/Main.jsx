1554087d16e879ffd242ef0f9032729b118efb60
kyachingprue <kyachingpruemarma.info@gmail.com>
2025-03-08 08:49:42 +0600

    first commit

---
+++ src/Main/Main.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Main = () => {
+  return (
+    <div>
+      <h3 className='font-bold text-4xl text-center my-8'>This is main Pages</h3>
+    </div>
+  );
+};
+
+export default Main;
