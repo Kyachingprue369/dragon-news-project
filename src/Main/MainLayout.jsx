fd911ba1c01427cc55a128755f97241b9d3a9fd5
kyachingprue <kyachingpruemarma.info@gmail.com>
2025-03-09 15:29:06 +0600

    new commit

--- src/Main/MainLayout.jsx
+++ src/Main/MainLayout.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Header from '../Components/Header/Header';
 import LatestNews from '../Components/Header/LatestNews';
+import Navbar from '../Components/Navbar/Navbar';
 
 const MainLayout = () => {
   return (
@@ -11,6 +12,14 @@ const MainLayout = () => {
       <section>
         <LatestNews></LatestNews>
       </section>
+      <section>
+        <Navbar></Navbar>
+      </section>
+      <main className='w-11/12 mt-20 mx-auto grid md:grid-cols-12 gap-5'>
+        <aside className='col-span-3 font-bold text-xl'>All Category</aside>
+        <section className='col-span-6 font-bold text-xl'>Dragon News Home</section>
+        <aside className='col-span-3 font-bold text-xl'>Login With</aside>
+      </main>
     </div>
   );
 };
